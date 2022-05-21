841f527e709fc8d024804fb35466bcb968fda7e3
JG Rempto <joao.rempto@gmail.com>
2022-05-21 15:47:46 -0300

    my first lesson

---
+++ variables/variables1.js
@@ -0,0 +1,14 @@
+const a = 1;
+console.log(a);
+console.log(typeof a);
+const b = 2;
+console.log(b);
+const c = a + b;
+console.log(c);
+const name = "JG";
+console.log(name);
+console.log(typeof name);
+const role = "gamer";
+console.log(name + role);
+console.log(name + " " + role);
+console.log(name, role);
